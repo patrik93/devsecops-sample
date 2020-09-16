ce959840da5803b9b913b215bd93f387d016e3ca
git_test <patrikhajnal93@gmail.com>
2020-09-16 15:20:32 +0200

    added sonarqube instance into the ec2 template

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -28,11 +28,30 @@ resource "aws_instance" "tomcat-instance" {
   }
 }
 
+resource "aws_instance" "sonar-instance" {
+  ami = var.ami-amazon
+  instance_type = "t2.medium" #Costs $0.05 per Hour in eu-west-1
+  #key_name = var.keyname
+  key_name = var.tmp-key
+  subnet_id = var.public-subnet
+  vpc_security_group_ids = ["sg-064e815cb0507f634"]
+  user_data = file("install_sonar.sh")
+
+  associate_public_ip_address = true
+  tags = {
+    Name = "Sonar-instance"
+  }
+}
+
+
 
 output "Jenkins-instance" {
   value = aws_instance.jenkins-instance.public_dns
 }
 output "Tomcat-instance" {
   value = aws_instance.tomcat-instance.public_dns
+}
+output "Sonar-instance" {
+  value = aws_instance.sonar-instance.public_dns
 }
 
