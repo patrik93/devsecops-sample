4774c32d93390b0a80032b90545d8b6e2ce1c71e
git_test <patrikhajnal93@gmail.com>
2020-09-21 13:57:24 +0200

    Modified owner of docker to ec2-user and added OWASP dependency checker docker image pull for further use from jenkins.

--- terraform/install_sonar.sh
+++ terraform/install_sonar.sh
@@ -3,9 +3,10 @@
 echo "Install Docker engine"
 sudo yum update -y
 sudo yum install docker -y
-sudo chkconfig docker on
-sudo service docker start
+sudo usermod -a -G docker ec2-user
+chkconfig docker on
+service docker start
 
-sudo docker run -d --name sonarqube -p 9000:9000 docker.io/library/sonarqube:7.9-community
-
+docker run -d --name sonarqube -p 9000:9000 docker.io/library/sonarqube:7.9-community
+docker pull owasp/dependency-check
 
